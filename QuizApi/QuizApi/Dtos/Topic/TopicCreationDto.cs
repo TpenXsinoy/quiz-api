926e43c1dbbe2e231e19aa26d8aa2642a58db96e
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-15 23:12:19 +0800

    Add topic dto

---
+++ QuizApi/QuizApi/Dtos/Topic/TopicCreationDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizApi.Dtos.Topic
+{
+    public class TopicCreationDto
+    {
+        [Required(ErrorMessage = "Topic name is required.")]
+        [MaxLength(50, ErrorMessage = "Maximum lenghth for the name of a topic is 50 characters.")]
+        public string? Name { get; set; }
+    }
+}
